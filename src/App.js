3282a34c9133a3647e9ec23abb4de14119d7f9d3
Cameron Yick <cameron.yick@enigma.com>
2018-10-20 16:40:49 -0400

    Get user's position from browser geolocation API

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import withLocation from './components/withLocation';
+
 class App extends Component {
   render() {
+    const { location } = this.props;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
+            {location.latitude}
+            {location.longitude}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default withLocation(App);
---
+++ src/components/withLocation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+// Harcoded as a fallback
+const NEW_YORK_LOCATION = {
+  latitude: 40.7238086,
+  longitude: -73.97646069999999,
+}
+
+const withLocation = (WrappedComponent) => {
+  return class extends React.Component {
+    state = {
+      location: NEW_YORK_LOCATION
+    }
+
+    componentDidMount() {
+      this.getLocationPromise()
+        .then(data => this.setState({location: data.coords}) )
+        .then(data => this.setState({ data }));
+    }
+
+    getLocationPromise = () => {
+      if (navigator) {
+        return new Promise(
+          (resolve, reject) => navigator.geolocation.getCurrentPosition(resolve, reject)
+        )
+      } else {
+        return new Promise(
+          resolve => resolve({
+            coords: NEW_YORK_LOCATION
+          })
+        )
+      }
+    }
+
+    getLocation = async () => {
+      const response = await this.getLocationPromise().then(result => result);
+      return response;
+    }
+
+    render() {
+      return  <WrappedComponent location={this.state.location} {...this.props}/>
+    }
+  }
+}
+
+
+export default withLocation;
