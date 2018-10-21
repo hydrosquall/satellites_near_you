156ba055d271ede40c70bb0c1cc70f9dbea63cef
Cameron Yick <cameron.yick@enigma.com>
2018-10-20 23:33:34 -0400

    Set satellite color based on user

--- src/components/sketches/satellites.js
+++ src/components/sketches/satellites.js
@@ -7,7 +7,35 @@ const Y_AXIS = 1;
 const X_AXIS = 2;
 const c1 = '#3c3b52';
 const c2 = '#252233';
-const sat_color = ['rgba(19,212,233,.6)', 'rgba(19,212,233,1)'];
+
+const WING_COLOR = '#d2d2d2';
+
+const USER_CATEGORY = {
+  commercial: 'Commercial',
+  government: 'Government',
+  military: 'Military',
+  civil: 'Civil',
+}
+
+const USER_COLOR_MAP = {
+  [USER_CATEGORY.commercial]: ['rgba(19,212,233,.6)', 'rgba(19,212,233,1)'],
+  [USER_CATEGORY.government]: ['rgba(255,232,83,.6)', 'rgba(255,232,83,1)'],
+  [USER_CATEGORY.military]: ['rgba(83,120,56,.6)', 'rgba(83,120,56,1)'],
+  [USER_CATEGORY.civil]: ['rgba(228,104,85,.6)', 'rgba(228,104,85,1)'],
+}
+
+const getSatelliteUser = (satellite) => {
+  const { users } = satellite;
+  const categories = Object.keys(USER_COLOR_MAP);
+
+  let finalCategory = USER_CATEGORY.commercial; // default
+  for (let i in categories) {
+    if (users.startsWith(categories[i])) {
+      return categories[i];
+    }
+  }
+  return finalCategory;
+}
 
 export default function sketch(p) {
   let sats = [];
@@ -29,16 +57,21 @@ export default function sketch(p) {
     p.textSize(20);
   };
 
-  const drawSatellite = (x, y) => {
+  const drawSatellite = (x, y, sat) => {
     const radius = 20;
     const wing = { 'w': 30, 'h': 18 };
 
-    p.fill('#d2d2d2');
-    p.rect(x - wing.w - radius / 2, y - (wing.h / 2), wing.w, wing.h, 3);
-    p.rect(x + radius / 2, y - (wing.h / 2), wing.w, wing.h, 3);
-    p.fill(p.color(sat_color[0]));
+    const user = getSatelliteUser(sat);
+    const color = USER_COLOR_MAP[user];
+
+    p.fill(WING_COLOR);
+
+    const yPos = y - (wing.h / 2);
+    p.rect(x - wing.w - radius / 2, yPos, wing.w, wing.h, 3);
+    p.rect(x + radius / 2, yPos, wing.w, wing.h, 3);
+    p.fill(p.color(color[0]));
     p.ellipse(x, y, radius, radius);
-    p.fill(p.color(sat_color[1]));
+    p.fill(p.color(color[1]));
     p.ellipse(x, y, radius * .8, radius * .8);
     p.fill('white');
     p.ellipse(x, y, 2, 2);
@@ -71,7 +104,7 @@ export default function sketch(p) {
     const y = sats.map(sat => yScale(sat.satalt));
 
     sats.forEach((sat, i) => {
-      drawSatellite(x[i], y[i]);
+      drawSatellite(x[i], y[i], sat);
     });
   };
 };
